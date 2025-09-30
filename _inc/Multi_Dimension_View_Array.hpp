3e2ce32ce64e29d8abd615f316557e587f1a39e4
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-09-30 23:05:19 +0200

    quick

--- _inc/Multi_Dimension_View_Array.hpp
+++ _inc/Multi_Dimension_View_Array.hpp
@@ -1,7 +1,8 @@
 #include "__preprocessor__.h"
+#include <span>
 
 // #define BOUND_CHECKS_Multi_Dimension_View_Array(...) __VA_ARGS__
-#define BOUND_CHECKS_Multi_Dimension_View_Array(...) 
+#define BOUND_CHECKS_Multi_Dimension_View_Array(...)
 
 template <typename T>
 class Multi_Dimension_View_Array
@@ -11,7 +12,10 @@ class Multi_Dimension_View_Array
     u64 DEPTH;
     vector<T> buffer;
 
-    bool bound_check(u64 x, u64 y, u64 z) const { return ((0 <= x && x < WIDTH) && (0 <= y && y < HEIGHT) && (0 <= z && z < DEPTH)) && bound_check(to_d1(x, y, z)); }
+    bool bound_check(u64 x, u64 y, u64 z) const
+    {
+        return ((0 <= x && x < WIDTH) && (0 <= y && y < HEIGHT) && (0 <= z && z < DEPTH)) && bound_check(to_d1(x, y, z));
+    }
     bool bound_check(u64 x, u64 y) const { return ((0 <= x && x < WIDTH) && (0 <= y && y < HEIGHT)) && bound_check(to_d1(x, y)); }
     bool bound_check(u64 x) const { return (0 <= x && x < buffer.size()); }
 
@@ -23,9 +27,9 @@ public:
 
     void set_sizes(i64 width, i64 height = 1, i64 depth = 1)
     {
-        if(width <= 0) FATAL_ERROR("WIDTH set with incorrect value");
-        if(height <= 0) FATAL_ERROR("HEIGHT set with incorrect value");
-        if(depth <= 0) FATAL_ERROR("DEPTH set with incorrect value");
+        if (width <= 0) FATAL_ERROR("WIDTH set with incorrect value");
+        if (height <= 0) FATAL_ERROR("HEIGHT set with incorrect value");
+        if (depth <= 0) FATAL_ERROR("DEPTH set with incorrect value");
 
         WIDTH = width;
         HEIGHT = height;
@@ -43,20 +47,22 @@ public:
     {
         BOUND_CHECKS_Multi_Dimension_View_Array(if (!bound_check(x, y, z)) FATAL_ERROR("accesing beyond vector bounds");)
 
-        return &buffer[to_d1(x, y, z)];
+            return &buffer[to_d1(x, y, z)];
     }
 
     T* get(u64 x, u64 y)
     {
         BOUND_CHECKS_Multi_Dimension_View_Array(if (!bound_check(x, y)) FATAL_ERROR("accesing beyond vector bounds");)
 
-        return &buffer[to_d1(x, y)];
+            return &buffer[to_d1(x, y)];
     }
 
     T* get(u64 x)
     {
         BOUND_CHECKS_Multi_Dimension_View_Array(if (!(x < buffer.size())) FATAL_ERROR("accesing beyond vector bounds");)
 
-        return &buffer[x];
+            return &buffer[x];
     }
+
+    std::span<const T> get_span() { return std::span<const T>(buffer); }
 };
